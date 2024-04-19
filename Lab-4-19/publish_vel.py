915917ed51e361755564be4b5e9d5556b3bbc74a
jeronstone <jstone14@terpmail.umd.edu>
2024-04-19 11:27:11 -0700

    init labnode

---
+++ Lab-4-19/publish_vel.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+class LabNode(Node):
+    def __init__(self):
+        super().__init__('lab_4_19')
+
+        self.scan_subscriber = self.create_subscription(LaserScan,'/scan',self.scan_subscriber_handler,10)
+        self.cmd_vel_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+
+        self.timer = self.create_timer(.1, self.timer_callback)
+    
+    def timer_callback(self):
+        pass
+
+    def scan_subscriber_handler(self, data):
+        pass
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    labnode = LabNode()
+
+    rclpy.spin(labnode)
+    
+    labnode.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
