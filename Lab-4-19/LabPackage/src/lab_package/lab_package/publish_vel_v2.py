013838a54a2fe75bf7a06d5b4479d01b2f04ed40
Sriman Selvakumaran <65560845+sselvakumaran@users.noreply.github.com>
2024-04-28 16:07:32 -0400

    Update publish_vel_v2.py

--- Lab-4-19/LabPackage/src/lab_package/lab_package/publish_vel_v2.py
+++ Lab-4-19/LabPackage/src/lab_package/lab_package/publish_vel_v2.py
@@ -5,18 +5,18 @@ from geometry_msgs.msg import Twist
 import numpy as np
 
 PI = np.pi
-DANGER = 0.5
-IDEAL = 0.75
-MAX = 1.5
+DANGER = 0.2
+IDEAL = 0.35
+MAX = 0.6
 SPEED = 0.5
-BACKUP = 0.25
+BACKUP = 0.1
 NORM = PI / 2
 FWD = PI
 NORMSEARCH_MIN = 0
-NORMSEARCH_MAX = PI * 1.5
+NORMSEARCH_MAX = PI * 1.25
 TO_RAD = PI / 360.0
-HOLD_WEIGHT = 0.3
-FOLLOW_WEIGHT = 0.7
+HOLD_WEIGHT = 2.0
+FOLLOW_WEIGHT = 2.0
 
 PUSH_CONST = IDEAL - DANGER
 PULL_CONST = MAX - IDEAL
@@ -35,25 +35,31 @@ class LabNode(Node):
       self.timer = self.create_timer(1.0/FPS, self.timer_callback)
   
   def timer_callback(self):
-    if not self.ranges:
+    if not hasattr(self, "ranges"):
       return
     twist = Twist()
     twist.linear.x = SPEED
     twist.linear.y = twist.linear.z = 0.0
     twist.angular.z = 0.0
     twist.angular.x = twist.angular.y = 0.0
-    casts = np.convolve(np.clip(self.ranges, 0.0, MAX), [0.2] * 5, mode = 'same')
+    casts = np.convolve(np.clip(self.ranges, 0.0, MAX+0.5), [0.2] * 5, mode = 'same')
     n_i = np.argmin(casts[SEARCHMIN_I : SEARCHMAX_I]) + SEARCHMIN_I
     c_i = np.argmin(casts)
-    norm = casts[n_i], close = casts[c_i]
+    norm = casts[n_i]
+    close = casts[c_i]
     if close <= DANGER:
+      self.get_logger().info("DANGER")
       theta = c_i * TO_RAD
-      direction = 1 if np.abs(theta - FWD) < PI / 2 else -1
-      twist.linear.x = BACKUP * -direction
-      twist.angular.z = (theta - FWD) * direction
+      lin_dir = np.cos(theta)
+      ang_dir = np.sin(theta)
+      self.get_logger().info(f"{np.abs(theta - FWD)}")
+      twist.linear.x = BACKUP * lin_dir
+      twist.angular.z = ang_dir
+      self.get_logger().info(f"{twist.linear.x} {twist.angular.z}")
       self.cmd_vel_publisher.publish(twist)
       return
     elif norm < MAX:
+      self.get_logger().info("FOLLOWING NORM")
       theta = n_i * TO_RAD
       twist.linear.x = SPEED
       twist.angular.z = FOLLOW_WEIGHT * (theta - NORM) \
@@ -61,7 +67,10 @@ class LabNode(Node):
             ((norm - IDEAL) / PUSH_CONST) ** 2 if norm < IDEAL \
             else -((norm - IDEAL) / PULL_CONST) ** 2)
     if np.abs(c_i * TO_RAD - FWD) > PI / 2 and close < IDEAL:
+      self.get_logger().info("LEFT IDEAL")
       twist.angular.z += HOLD_WEIGHT * -((close - IDEAL) / PUSH_CONST) ** 2
+    self.get_logger().info(f"{twist.linear.x} {twist.angular.z}")
+    twist.linear.x = 1 - (2*twist.angular.z / PI) ** 2
     self.cmd_vel_publisher.publish(twist)
       
   # ranges[0] -> back
