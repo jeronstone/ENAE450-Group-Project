40722427efcecd517fa8476622e875494fdc7d0f
bnewman6 <bnewman6@terpmail.umd.edu>
2024-05-12 17:35:02 -0400

    idk

--- build/hw5/build/lib/hw5/wallrider_v1.py
+++ build/hw5/build/lib/hw5/wallrider_v1.py
@@ -8,29 +8,31 @@ import numpy as np
 
 PI = np.pi  # pi
 DANGER = 0.1625  # unsafe wall radius (m)
+DELIBERATION = 0.1  # distance of deliberation
 IDEAL = 0.2  # ideal wall following radius (m)
 MAX = 0.5  # max search radius (m)
 SPEED = 0.5  # normal speed (m/s)
 BACKUP = 0.1  # slower speed for backing up / adjusting (m/s)
+# direction of wall we should be following, e.g. right wall (rad)
+NORM = PI / 2
 FWD = PI  # direction robot is going (180 degrees) (rad)
+# search between MIN and MAX for closest raycast on the right
+NORMSEARCH_MIN = PI * 0.25
+NORMSEARCH_MAX = PI * 0.85
 # search between MIN and MAX for closest raycast in front
-FWDSEARCH_MIN = PI * 0.90
-FWDSEARCH_MAX = PI * 1.10
+FWDSEARCH_MIN = PI * 0.85
+FWDSEARCH_MAX = PI * 1.05
 # raycast array index to radians conversion factor
 TO_RAD = PI / 360.0
 HOLD_WEIGHT = 1.0  # angular weighting to consider maintaining distance from wall
 FOLLOW_WEIGHT = 2.0  # angular weighting to consider following the wall in the first place
-# direction of wall we should be following, e.g. right wall (rad)
-NORM = PI
-# search between MIN and MAX for closest raycast on the right
-NORMSEARCH_MIN = PI * 0.45
-NORMSEARCH_MAX = PI * 0.45
-NORMSEARCHMIN_I = int(NORMSEARCH_MIN / TO_RAD)
-NORMSEARCHMAX_I = int(NORMSEARCH_MAX / TO_RAD)
+
 # some precalculations here
 PUSH_CONST = (IDEAL - DANGER) * 0.75
 PULL_CONST = (MAX - IDEAL) * 0.4
 
+NORMSEARCHMIN_I = int(NORMSEARCH_MIN / TO_RAD)
+NORMSEARCHMAX_I = int(NORMSEARCH_MAX / TO_RAD)
 FWDSEARCHMIN_I = int(FWDSEARCH_MIN / TO_RAD)
 FWDSEARCHMAX_I = int(FWDSEARCH_MAX / TO_RAD)
 
@@ -39,7 +41,7 @@ FPS = 10.0
 
 class LabNode(Node):
     def __init__(self):
-        super().__init__('wallrider_v2')
+        super().__init__('wallrider')
         self.get_logger().info("starting")
         # read from scan
         self.scan_subscriber = self.create_subscription(
@@ -47,8 +49,6 @@ class LabNode(Node):
         # publish movement every so often
         self.cmd_vel_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.timer = self.create_timer(0.01, self.timer_callback)
-        self.last_turn = None
-        self.right = -1.0
 
     def timer_callback(self):
         # if we haven't gotten a raycast yet, don't calculate anything
@@ -69,24 +69,31 @@ class LabNode(Node):
         # this is to get the angle from it to do calculations
         # n_i / norm: index / distance from wall
         # f_i / fwd: index / distance from foward wall
-        # c_i / close: index / distance from closest object (in any direction)
+        # c_i / norm: index / distance from closest object (in any direction)
         n_i = np.argmin(
-            casts[int(252 - self.right * NORMSEARCHMIN_I): int(468 - self.right * NORMSEARCHMAX_I)]) + int(252 - self.right * NORMSEARCHMIN_I)
-        c_i = np.argmin(casts)
+            casts[NORMSEARCHMIN_I: NORMSEARCHMAX_I]) + NORMSEARCHMIN_I
+        # if the distance to the left and right is about the same, focus on the right
+        if casts[np.argmin(casts[90:330])] - casts[np.argmin(casts[390:630])] < DELIBERATION:
+            # Takes the closest thing on the right half
+            c_i = np.argmin(casts[:360])
+        else:  # if it is not deciding between two walls, just take the closest wall
+            c_i = np.argmin(casts)
         f_i = np.argmin(casts[FWDSEARCHMIN_I: FWDSEARCHMAX_I]) + FWDSEARCHMIN_I
         norm = casts[n_i]
-        self.get_logger().info(f"norm: {norm}")
         close = casts[c_i]
         fwd = casts[f_i]
+        self.get_logger().info(f'{close}')
         if close <= DANGER:  # if anything is in our danger zone
             theta = c_i * TO_RAD
             # move in the opposite direction, 0 if on left/right
             lin_dir = np.cos(theta) ** 3
             ang_dir = np.sin(theta)  # turn away if on the side, 0 if fwd/bkwd
+            self.get_logger().info(f"{fwd} {f_i * TO_RAD}")
             # move speed relative to BACKUP and our constants calculated earlier
             twist.linear.x = BACKUP * lin_dir
             twist.angular.z = ang_dir
-            self.get_logger().info("DANGER!")
+            self.get_logger().info(
+                f"DANGER! {twist.linear.x} {twist.angular.z}")
             # publish and quit (do not consider anything else)
             self.cmd_vel_publisher.publish(twist)
             return
@@ -95,11 +102,11 @@ class LabNode(Node):
             self.get_logger().info("FWD TURN")
             theta = f_i * TO_RAD  # calculate angle of raycast
             twist.linear.x = BACKUP  # use BACKUP speed
-            # theta - NORM is the angular velocity we need to turn to make NORM on our right/left
-            twist.angular.z = self.right * FOLLOW_WEIGHT * (theta - (NORM + self.right * PI / 2))
+            # theta - NORM is the angular velocity we need to turn to make NORM on our right
+            twist.angular.z = FOLLOW_WEIGHT * (theta - NORM)
             self.cmd_vel_publisher.publish(twist)
             return
-        # if there is anything in view that is on our right/left, follow it
+        # if there is anything in view that is on our right, follow it
         if norm < MAX:
             self.get_logger().info("FOLLOWING NORM")
             theta = n_i * TO_RAD
@@ -107,16 +114,24 @@ class LabNode(Node):
             # scale angular velocity to turn it perp. from wall
             # plus quadratic scaling to maintain ideal distance from wall
             # quadratic -> scales more for objects far away from ideal
-            twist.angular.z = self.right * (FOLLOW_WEIGHT * (theta - (NORM + self.right * PI / 2)) \
+            twist.angular.z = FOLLOW_WEIGHT * (theta - NORM) \
                 + HOLD_WEIGHT * (
                 ((norm - IDEAL) / PUSH_CONST) ** 2 if norm < IDEAL
-                else -((norm - IDEAL) / PULL_CONST) ** 2))
+                else -((norm - IDEAL) / PULL_CONST) ** 2)
+
+            v1 = HOLD_WEIGHT * (
+                ((norm - IDEAL) / PUSH_CONST) ** 2 if norm <= IDEAL
+                else -((norm - IDEAL) / PULL_CONST) ** 2)
+            self.get_logger().info(
+                f"follow {FOLLOW_WEIGHT * (theta - NORM)} hold {v1}")
+        # if there is something on our left and within threatening distance, turn away from it
+        if np.abs(c_i * TO_RAD - FWD) > PI / 2 and close < IDEAL:
+            self.get_logger().info("LEFT IDEAL")
+            twist.angular.z += 0.0 * HOLD_WEIGHT * \
+                ((close - IDEAL) / PUSH_CONST) ** 2
+        self.get_logger().info(f"{twist.linear.x} {twist.angular.z}")
         # scale so that if turning is urgent, reduce linear speed
-        twist.linear.x *= abs(1 - (2*twist.angular.z / PI) ** 2)
-        '''if self.last_turn and np.abs(casts[540] - casts[180]) > 0.2 and np.abs(twist.angular.z - self.last_turn) > PI / 2:
-            twist.angular.z = self.last_turn
-            self.right *= -1.0
-        self.last_turn = twist.angular.z'''
+        twist.linear.x *= 1 - (2*twist.angular.z / PI) ** 2
         # twist.angular.z -= 0.25 * PI * norm
         self.cmd_vel_publisher.publish(twist)
 
